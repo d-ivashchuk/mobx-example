1c6a5b2314dd9c199234bc74f4d0206e11c11d35
Dimitri Ivashchuk <dimaiv@me.com>
2019-01-02 17:42:44 +0100

    wip

--- src/Components/CustomField.js
+++ src/Components/CustomField.js
@@ -4,16 +4,15 @@ import React from 'react';
 import { FormControl, ControlLabel } from 'react-bootstrap';
 import { connect, getIn } from 'formik';
 
-const CustomField = ({ field, name, form: { touched, errors }, ...props }) => {
-  console.log(field);
-  const error = getIn(props.formik.errors, field.name);
-  const touch = getIn(props.formik.touched, field.name);
-  const value = field.value;
+const CustomField = ({ field, form: { touched, errors }, ...props }) => {
+  console.log(field.name);
   return (
     <div>
       <ControlLabel>{field.name}</ControlLabel>
-      <FormControl type="text" value={value} {...props} />
-      {touch && error && <div className="error">{error}</div>}
+      <FormControl type="text" value={field.value} {...props} />
+      {touched[field.name] && errors[field.name] && (
+        <div className="error">{errors[field.name]}</div>
+      )}
     </div>
   );
 };
